06a6464226dea3026e16542f89cca762bef7213b
Shubham M Chauhan <shubhamchauhan0903@gmail.com>
2025-06-18 00:21:17 +0530

    Practice Demo class added to dev branch

---
+++ src/Basics/PracticeDemo.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class PracticeDemo {
+	
+	public void add(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PracticeDemo p = new PracticeDemo();
+		p.add(5, 7);
+	}
+
+}
